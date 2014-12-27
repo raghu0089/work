6a57cfaff61002f812c267b709f9bbf92dc71e2d
Raghav <bhadra.illa@iglulabs.com>
2014-12-27 05:52:02 +0530

    Last commit probably

--- mobile.py
+++ mobile.py
@@ -5,9 +5,10 @@ import re
 
 word=(raw_input("Enter your input : "))
 
-urls=['http://www.flipkart.com/mobiles/pr?sid=tyy%2C4io&ref=db931487-4b7b-496a-95f5-088473113c0c',
-      'http://www.amazon.in/s/ref=sr_nr_n_1/275-7664783-0000827?fst=as%3Aoff&rh=n%3A976419031%2Cn%3A!976420031%2Cn%3A1389401031%2Cn%3A1389432031%2Cp_n_operating_system_browse-bin%3A1485077031%2Cn%3A1805560031&bbn=1389432031&ie=UTF8&qid=1419456185&rnid=1389432031',
-      'http://www.snapdeal.com/products/mobiles-mobile-phones'
+urls=[
+      'http://www.flipkart.com/mobiles/pr?q='+word+'&as=on&as-show=on&otracker=start&sid=tyy%2C4io&as-pos=1_1_ic_nexus',
+      'http://www.amazon.in/s/ref=sr_ex_n_1?rh=n%3A976419031%2Cn%3A1389401031%2Cn%3A1389432031%2Ck%3A'+word+'&bbn=1389432031&keywords='+word+'&ie=UTF8&qid=1419634271',
+      'http://www.snapdeal.com/search?keyword='+word+'&santizedKeyword=&catId=&categoryId=175&suggested=true&vertical=p&noOfResults=20&clickSrc=suggested&lastKeyword=&prodCatId=&changeBackToAll=true&foundInAll=false&categoryIdSearched=&cityPageUrl=&url=&utmContent=&catalogID=&dealDetail='
       ]
 for link in urls:
     response = requests.get(link)
@@ -33,27 +34,33 @@ for link in urls:
     elif domain=='http://www.amazon.in/':
         print "\n"
         i=0
+        element=parsed_body.xpath('//div[@class="s-item-container"]')
         total = len(parsed_body.xpath('//div[@class="s-item-container"]'))
         print "Amazon : \n" 
         while i< total:
+               
             title  = parsed_body.xpath('//div[@class="s-item-container"]/div[@class="a-row a-spacing-mini"]/div/a//h2/text()')[i]
             if word.lower() in title.lower():
-                rating  = parsed_body.xpath('//div[@class="s-item-container"]/div[@class="a-row a-spacing-none"]/span/span/a/i/span/text()')[i]
-                price = parsed_body.xpath('//div[@class="s-item-container"]/div[@class="a-row a-spacing-mini"]/div[@class="a-row a-spacing-none"]/a/span[@class="a-size-base a-color-price s-price a-text-bold"]/text()')[i]
-                print title+'-'+rating+'-'+price
+                rating  = parsed_body.xpath('//div[@class="s-item-container"]/div[@class="a-row a-spacing-none"]/span/span/a/i/span/text()')[i]                
+                check_price= element[i].xpath('.//div[@class="a-row a-spacing-mini"]/div[@class="a-row a-spacing-none"]/a/span[@class="a-size-base a-color-price s-price a-text-bold"]/text()')
+                if not check_price:
+                    print title+'-'+rating+'- Not Available'
+                else:
+                    print title+'-'+rating+'-'+check_price[0]
+                
             i=i+1
         
     elif domain=='http://www.snapdeal.com/':
         print "\n"
         i=0
-        total = len(parsed_body.xpath('//div[@class="product_listing_cont"]'))
+        total = len(parsed_body.xpath('//div[@class="hoverProductWrapper product-txtWrapper  "]'))
         print "Snapdeal : \n" 
         while i< total:
-            title  = parsed_body.xpath('//div[@class="product_listing_heading"]/a/text()')[i]
+            title  = parsed_body.xpath('//div[@class="product-title"]/a/text()')[i]
             if word.lower() in title.lower(): 
-                price  = parsed_body.xpath('//div[@class="product_listing_price_outer"]/div/span/text()')[i]
-                price=re.findall(r'\d+', price)[0]
-                print title.replace('\n','').replace('\t','')+'- Cannot scrape rating- Rs-'+price
+                price  = parsed_body.xpath('//a[@id="prodDetails"]/div[@class="product-price"]/div/span[@id="price"]/text()')[i]
+                rating  = parsed_body.xpath('//a[@id="prodDetails"]/div[@class="ratingsWrapper"]/div[@class="ratingStarsSmall"]/@ratings')[i]
+                print title.replace('\n','').replace('\t','')+'-'+rating+'-'+price
             i=i+1
                         
                         
